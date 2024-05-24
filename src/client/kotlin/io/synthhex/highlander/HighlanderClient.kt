c56ae09b8a6514612ebdaa7e56ab643e7db1928c
Synthhex <hexadecimal.overflow@gmail.com>
2024-05-24 15:43:11 +0200

    feat: init commit

---
+++ src/client/kotlin/io/synthhex/highlander/HighlanderClient.kt
@@ -0,0 +1,9 @@
+package io.synthhex.highlander
+
+import net.fabricmc.api.ClientModInitializer
+
+object HighlanderClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/io/synthhex/highlander/HighlanderDataGenerator.kt
@@ -0,0 +1,9 @@
+package io.synthhex.highlander
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object HighlanderDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
